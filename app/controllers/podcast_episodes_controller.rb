aea01e2ad72f8e17f9f02338a399c4a834ed6edd
Adam Frej <awfrej@googlemail.com>
2018-08-23 19:56:30 -0400

    controller generated with index and show methods working and nav hyperlink working, need to seed with examples to test"
    "

--- app/controllers/podcast_episodes_controller.rb
+++ app/controllers/podcast_episodes_controller.rb
@@ -1,2 +1,22 @@
 class PodcastEpisodesController < ApplicationController
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def index
+    @podcast_episodes = Podcast_episode.all
+  end
+
+  def show
+    @podcast_episode = Podcast_episode.find(params[:id])
+  end
+
+  def edit
+
+  end
+
 end
